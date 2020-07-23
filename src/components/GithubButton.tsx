10db51be86108f96d5e43ac002a05bdfbc112921
nshun <n.shun1110@gmail.com>
2020-07-23 14:26:14 +0900

    migrate react hooks

--- src/components/GithubButton.tsx
+++ src/components/GithubButton.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
 
-import {
-  Button,
-  createStyles,
-  Theme,
-  withStyles,
-  WithStyles,
-} from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 
 import { grey } from '@material-ui/core/colors';
 
-const styles = (theme: Theme) =>
-  createStyles({
-    githubButton: {
-      textTransform: 'none',
-      color: theme.palette.getContrastText(grey[900]),
-      backgroundColor: grey[900],
-      '&:hover': {
-        color: theme.palette.getContrastText(grey[100]),
-        backgroundColor: grey[100],
-      },
-      margin: theme.spacing(),
+const useStyle = makeStyles((theme) => ({
+  githubButton: {
+    textTransform: 'none',
+    color: theme.palette.getContrastText(grey[900]),
+    backgroundColor: grey[900],
+    '&:hover': {
+      color: theme.palette.getContrastText(grey[100]),
+      backgroundColor: grey[100],
     },
-  });
+    margin: theme.spacing(),
+  },
+}));
 
-function GithubButton(props: Readonly<WithStyles<typeof styles>>) {
-  const { classes } = props;
+const GithubButton = () => {
+  const classes = useStyle();
 
   return (
     <Button
@@ -39,6 +32,6 @@ function GithubButton(props: Readonly<WithStyles<typeof styles>>) {
       Github
     </Button>
   );
-}
+};
 
-export default withStyles(styles)(GithubButton);
+export default GithubButton;
