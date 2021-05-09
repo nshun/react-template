1290852dad308c210bc4b691641f02d3e4c9bd39
nshun <nshun@users.noreply.github.com>
2021-05-09 14:31:03 +0000

    deploy: 9c5610b945fbaf430f2dae92c132f0564e9cffbc

---
+++ precache-manifest.7d3c3a2589d40ac1234cf1a07966ac5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc429e30b863f0bf674c405f0fc9978",
+    "url": "https://nshun.github.io/react-template/index.html"
+  },
+  {
+    "revision": "9b7f2c9414df65070fba",
+    "url": "https://nshun.github.io/react-template/static/js/0.78951aa9.chunk.js"
+  },
+  {
+    "revision": "d7a49598467e4a5f1cbc",
+    "url": "https://nshun.github.io/react-template/static/js/3.4a16f6a7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://nshun.github.io/react-template/static/js/3.4a16f6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57b4333430be1dce2f2",
+    "url": "https://nshun.github.io/react-template/static/js/4.e6f55009.chunk.js"
+  },
+  {
+    "revision": "194176f1434efc1ce47c",
+    "url": "https://nshun.github.io/react-template/static/js/5.10e3d89e.chunk.js"
+  },
+  {
+    "revision": "9d08117bbbe3de09e66c",
+    "url": "https://nshun.github.io/react-template/static/js/main.8fa3db11.chunk.js"
+  },
+  {
+    "revision": "f4b539bc432ba6031a1d",
+    "url": "https://nshun.github.io/react-template/static/js/runtime-main.96fe7b3a.js"
+  }
+]);
